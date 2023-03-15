526dc1eef1985c649a9c0d98fd3d5ec42c3b1885
Neha <nehakhedkar30@gmail.com>
2023-03-15 21:52:40 +0530

    numpy - 15/03/23

---
+++ Prac_Problem_10.py
@@ -0,0 +1,12 @@
+''' You are given a 2-D array with dimensions X.
+Your task is to perform the min function over axis  and then find the max of that. '''
+
+import numpy as np
+n, m = map(int, input("kk : ").split())
+
+nlis = [list(map(int, input().split("list :"))) for _ in range(n)]
+narr = np.array(nlis)
+print(narr,":::::::")
+narr.reshape(n,m)
+narr = np.min(narr, axis = 1)
+print(np.max(narr))
